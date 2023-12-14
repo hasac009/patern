1c3ccd4da6dc2788837abe74ab5fd8774adf4b2d
Shunks <nazar.n2812@gmail.com>
2023-12-14 22:42:21 +0200

    first commit

---
+++ Singleton.Conceptual/NonThreadSafe/Program.cs
@@ -0,0 +1,51 @@
+﻿
+
+using System;
+
+namespace RefactoringGuru.DesignPatterns.Singleton.Conceptual.NonThreadSafe
+{
+    
+    public sealed class Singleton
+    {
+        
+        private Singleton() { }
+
+        
+        private static Singleton _instance;
+
+        
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+
+       
+        public void someBusinessLogic()
+        {
+            
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+        }
+    }
+}
